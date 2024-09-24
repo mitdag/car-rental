9de615228f78ffd9404090a7d79d78325464d9cd
Mithat <mithat@HP-ENVY-UBUNTU>
2024-09-24 17:11:31 +0200

    update login

--- app/auth/login.py
+++ app/auth/login.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from app.auth import oauth2
 from app.core import database
 from app.schemas.enums import LoginMethod
-from app.services import user
+from app.services import user as user_service
 from app.utils.hash import Hash
 
 router = APIRouter(
@@ -16,19 +16,24 @@ router = APIRouter(
 @router.post("/")
 def create_access_token(
         email: str = Body(...),
-        password: str = Body(),
-        login_method: LoginMethod = Body(),
+        password: str = Body(None),
+        login_method: LoginMethod = Body(...),
         db: Session = Depends(database.get_db)):
     if login_method == LoginMethod.EMAIL:
         if not password or password == "":
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials.No password")
 
         try:
-            app_user = user.get_user_by_email(email, db)
+            app_user = user_service.get_user_by_email(email, db)
             if not app_user or not Hash.verify(password, app_user.password):
                 raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
         except:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials")
+    else:
+        user = user_service.get_user_by_email(email, db).first()
+        if not user:
+            pass
+
     access_token = oauth2.create_access_token({"username": email})
     return {
         "access_token": access_token,
--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
-
-from fastapi import FastAPI
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from app.routers import car, user
 from app.auth import signup, login
 from app.core.database import Base
 from app.core.database import engine
+from app.routers import car
 from app.routers import user
 
 app = FastAPI()
@@ -27,4 +25,3 @@ app.mount(
 # This code is here to run the app from pycharm
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-
