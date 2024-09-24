165910c0896269185e5354579028ab9717f12c8b
Selman <selmank@gmail.com>
2024-09-24 13:07:04 +0200

    Create car model

---
+++ app/models/car.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Boolean, Column, Float, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import relationship
+
+from app.core.database import Base
+
+
+class DBCar(Base):
+    __tablename__ = "cars"
+    
+    car_id = Column(Integer, primary_key=True, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    make = Column(String, nullable=False)
+    model = Column(String, nullable=False)
+    year = Column(Integer, nullable=False)
+    transmission_type = Column(String, nullable=False)
+    motor_type = Column(String, nullable=False)
+    price_per_day = Column(Float, nullable=False)
+    description = Column(Text)
+    is_listed = Column(Boolean, default=True)
+
+    owner = relationship("DBUser", back_populates="cars")
