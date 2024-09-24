525dc69feaa92449da67aeaa660a5acae0c59cae
Mithat <mithat@HP-ENVY-UBUNTU>
2024-09-24 14:52:25 +0200

    Update hash.py

--- app/utils/hash.py
+++ app/utils/hash.py
@@ -1,13 +1,11 @@
-
-
 from passlib.context import CryptContext
 
 ctx = CryptContext(schemes="bcrypt", deprecated="auto")
 
 
 class Hash:
-    def bcrypt(self,plain_password: str):
+    def bcrypt(plain_password: str):
         return ctx.hash(plain_password)
 
-    def verify(self, plain_password: str, encrypted_password: str):
+    def verify(plain_password: str, encrypted_password: str):
         return ctx.verify(plain_password, encrypted_password)
