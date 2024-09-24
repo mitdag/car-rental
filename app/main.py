a2feda5c765380a42b500359525ecf3714c8bfe8
Selman <selmank@gmail.com>
2024-09-24 14:58:00 +0200

    Add car router to the main

--- app/main.py
+++ app/main.py
@@ -1,21 +1,19 @@
-
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
 
 from app import core
-from app.routers import user
 from app.auth import signup
-from app.core.database import Base
-from app.core.database import engine
+from app.core.database import Base, engine
+from app.routers import car, user
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(signup.router)
+app.include_router(car.router)
 
 Base.metadata.create_all(engine)
 
 # This code is here to run the app from pycharm
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-
