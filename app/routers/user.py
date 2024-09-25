ffd1708fd42e9c385a3986f4a3ca0f6125cf5b73
Mithat <mithat@HP-ENVY-UBUNTU>
2024-09-25 16:49:16 +0200

    Create signup_confirmation.html

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
+from app.auth import oauth2
 from app.core import database
 from app.schemas.user import UserProfile
 from app.services import user
@@ -8,8 +9,15 @@ from app.services import user
 router = APIRouter(prefix="/user", tags=["user"])
 
 
-@router.post("/profile")
+@router.put("/profile")
 def modify_user_profile(
-    user_profile: UserProfile, db: Session = Depends(database.get_db)
+        user_profile: UserProfile,
+        db: Session = Depends(database.get_db),
+        current_user=Depends(oauth2.get_current_user)
 ):
     return user.modify_user_profile(user_profile, db)
+
+
+@router.delete("/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(database.get_db)):
+    return user.delete_user(user_id, db)
