3febcc2a098eceb25b2fdb26e3e527289b7c13c5
Selman <selmank@gmail.com>
2024-09-30 13:24:37 +0200

    feat(auth): add email format validation to signup endpoint

--- app/auth/signup.py
+++ app/auth/signup.py
@@ -1,12 +1,12 @@
-from fastapi import APIRouter, Depends, Body, status, Request, Query
-from fastapi.responses import Response, HTMLResponse
+from fastapi import APIRouter, Body, Depends, Query, Request, status
+from fastapi.responses import HTMLResponse, Response
 from fastapi.templating import Jinja2Templates
+from pydantic import EmailStr
 from sqlalchemy.orm import Session
 
 from app.core import database
 from app.schemas import user as user_schema
 from app.services import user as user_service
-
 from app.utils import email_sender
 
 router = APIRouter(prefix="/signup", tags=["signup/login"])
@@ -16,9 +16,9 @@ templates = Jinja2Templates(directory="app/static/templates")
 
 @router.post("/", response_model=user_schema.UserDisplay)
 def signup(
-        email: str = Body(...),
-        password: str = Body(...),
-        db: Session = Depends(database.get_db),
+    email: EmailStr = Body(...),
+    password: str = Body(...),
+    db: Session = Depends(database.get_db),
 ):
     entry = user_service.create_signup_validation_entry(email, password, db)
     email_sender.send_signup_confirmation_email(
@@ -34,12 +34,14 @@ def signup(
 
 @router.get("/confirm", response_class=HTMLResponse)
 def signup_confirmation(
-        request: Request,
-        confirmation_id: int = Query(...),
-        key: str = Query(...),
-        db: Session = Depends(database.get_db),
+    request: Request,
+    confirmation_id: int = Query(...),
+    key: str = Query(...),
+    db: Session = Depends(database.get_db),
 ):
-    result = user_service.create_signup_user_from_confirmation_mail(confirmation_id, key, db)
+    result = user_service.create_signup_user_from_confirmation_mail(
+        confirmation_id, key, db
+    )
     if result["result"]:
         title = "Signup Completed"
         message = "Thank you for signing up. You can login and enjoy our app now!"
