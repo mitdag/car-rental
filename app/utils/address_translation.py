b3fce7151192f8ecf8e9686a86a45580004403f4
Selman <selmank@gmail.com>
2024-10-04 18:38:34 +0200

    refactor(address): enhance address_to_lat_lon to raise AddressValidationError for geocoding failures

--- app/utils/address_translation.py
+++ app/utils/address_translation.py
@@ -7,13 +7,17 @@ from app.utils.logger import logger
 geolocator = Nominatim(user_agent="car-rental", timeout=3)
 
 
+class AddressValidationError(Exception):
+    pass
+
+
 def address_to_lat_lon(address: Dict):
     try:
         location = geolocator.geocode(address)
         if location:
             return {"latitude": location.latitude, "longitude": location.longitude}
         else:
-            return {"latitude": None, "longitude": None}
+            raise AddressValidationError("Invalid address: unable to geocode.")
     except Exception as exc:
         logger.error(exc)
-        return {"latitude": None, "longitude": None}
+        raise AddressValidationError(f"Error occurred during geocoding: {exc}")
